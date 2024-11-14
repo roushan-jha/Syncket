eac72f7b9fc9e07571652c71aa26a31aaa719b17
roushan-jha <jharoushan20@gmail.com>
2024-11-15 00:24:00 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (  
+    <div>
+      <h2 className='font-bold text-blue-800'>SocketSync</h2>
+      <Button>Let's Get Started</Button>
+    </div>
+  )
+}
+
+export default App
