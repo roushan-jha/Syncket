bae3d6a7606546e54272b64f685afc3dcec29eba
roushan-jha <jharoushan20@gmail.com>
2024-11-16 02:23:11 +0530

    basic-app-structure and added-assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 const App = () => {
   return (  
-    <div>
-      <h2 className='font-bold text-blue-800'>SocketSync</h2>
-      <Button>Let's Get Started</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<Navigate to='/auth' />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
