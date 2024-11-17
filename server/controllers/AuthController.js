3972af624e5391db472fd0c5208132ffd0bb16ff
roushan-jha <jharoushan20@gmail.com>
2024-11-18 00:25:34 +0530

    jwt verification

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -68,3 +68,25 @@ export const login = async (req, res, next) => {
         return res.status(500).send("Internal Server Error");
     }
 }
+
+export const getUserInfo = async (req, res, next) => {
+    try {
+        const userData = await User.findById(req.userId);
+        if(!userData) {
+            return res.status(404).send("User not found!");
+        }
+
+       return res.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color
+       })
+    } catch (err) {
+        console.log({err});
+        return res.status(500).send("Internal Server Error");
+    }
+}
