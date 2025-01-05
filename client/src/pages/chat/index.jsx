cffb396c830c4e03ac28f8c0eb8e78cb6c86f113
roushan-jha <jharoushan20@gmail.com>
2025-01-05 23:54:42 +0530

    chat-layout

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -2,6 +2,9 @@ import { useAppStore } from "@/store"
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
 
@@ -15,7 +18,11 @@ const Chat = () => {
     }
   }, [userInfo, navigate]);
   return (
-    <div>Chat</div>
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactsContainer />
+      {/* <EmptyChatContainer /> */}
+      <ChatContainer />
+    </div>
   )
 }
 
