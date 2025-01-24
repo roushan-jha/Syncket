2c354641c902f41239e5577aed6bfa7eb3db5aa4
roushan-jha <jharoushan20@gmail.com>
2025-01-24 15:16:50 +0530

    socket.io-client setup

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,39 @@
+import { useAppStore } from "@/store";
+import { HOST } from "@/utils/constants";
+import { io } from "socket.io-client";
+
+import { createContext, useContext, useRef, useEffect } from "react";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }) => {
+  const socket = useRef();
+  const { userInfo } = useAppStore();
+
+  useEffect(() => {
+    if (userInfo) {
+      socket.current = io(HOST, {
+        withCredentials: true,
+        query: { userId: userInfo.id },
+      });
+
+      socket.current.on("connect", () => {
+        console.log("Connected to socket server");
+      });
+
+      return () => {
+        socket.current.disconnect();
+      };
+    }
+  }, [userInfo]);
+
+  return (
+    <SocketContext.Provider value={socket.current}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Toaster } from 'sonner'
+import { Toaster } from './components/ui/sonner.jsx'
+import { SocketProvider } from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <>
+  <SocketProvider>
     <App />
     <Toaster closeButton />
-  </>
+  </SocketProvider>
 )
