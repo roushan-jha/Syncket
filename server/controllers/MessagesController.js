3223ccd204b3f8c7334f6131b89be53823479696
roushan-jha <jharoushan20@gmail.com>
2025-02-07 01:04:02 +0530

    upload-files on server

--- server/controllers/MessagesController.js
+++ server/controllers/MessagesController.js
@@ -1,4 +1,5 @@
 import Message from "../models/Messages.js";
+import { mkdirSync, renameSync } from "fs";
 
 export const getMessages = async (req, res, next) => {
   try {
@@ -10,13 +11,33 @@ export const getMessages = async (req, res, next) => {
     }
 
     const messages = await Message.find({
-        $or: [
-            { sender: user1, recipient: user2 },
-            { sender: user2, recipient: user1 }
-        ],
+      $or: [
+        { sender: user1, recipient: user2 },
+        { sender: user2, recipient: user1 },
+      ],
     }).sort({ timestamp: 1 });
 
     return res.status(200).json({ messages });
+  } catch (err) {
+    console.log({ err });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const uploadFile = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send("File is required.");
+    }
+    const date = Date.now();
+    let fileDir = `uploads/files/${date}`;
+    let fileName = `${fileDir}/${req.file.originalname}`;
+
+    mkdirSync(fileDir, { recursive: true });
+
+    renameSync(req.file.path, fileName);
+
+    return res.status(200).json({ filePath: fileName });
   } catch (err) {
     console.log({ err });
     return res.status(500).send("Internal Server Error");
