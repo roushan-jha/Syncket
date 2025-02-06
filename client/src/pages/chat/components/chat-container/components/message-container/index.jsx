b1856d4b9e5ba1423a74a3ffab2079e4c6629244
roushan-jha <jharoushan20@gmail.com>
2025-02-07 01:03:15 +0530

    send-files UI

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -1,8 +1,10 @@
 import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
-import { GET_ALL_MESSAGES_ROUTES } from "@/utils/constants";
+import { GET_ALL_MESSAGES_ROUTES, HOST } from "@/utils/constants";
 import moment from "moment";
 import { useEffect, useRef } from "react";
+import { MdFolderZip } from "react-icons/md";
+import { IoMdArrowRoundDown } from "react-icons/io";
 
 const MessageContainer = () => {
   const scrollRef = useRef();
@@ -41,6 +43,14 @@ const MessageContainer = () => {
     }
   }, [selectedChatMessages]);
 
+  const checkIfImage = (filePath) => {
+    const imageRegex =
+      /\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg|ico|heic|heif)$/i;
+    return imageRegex.test(filePath);
+  };
+
+  const downloadFile = (file) => {};
+
   const renderMessages = () => {
     let lastDate = null;
     return selectedChatMessages.map((message, index) => {
@@ -76,6 +86,38 @@ const MessageContainer = () => {
             } border inline-block px-4 py-2 rounded my-1 max-w-[50%] break-words`}
           >
             {message.content}
+          </div>
+        )}
+        {message.messageTypes === "file" && (
+          <div
+            className={`${
+              message.sender !== selectedChatData._id
+                ? "bg-[#8417ff]/5 text-[8417ff]/90 border-[#8417ff]/50"
+                : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
+            } border inline-block px-4 py-2 rounded my-1 max-w-[50%] break-words`}
+          >
+            {checkIfImage(message.fileUrl) ? (
+              <div className="cursor-pointer">
+                <img
+                  src={`${HOST}/${message.fileUrl}`}
+                  height={300}
+                  width={300}
+                />
+              </div>
+            ) : (
+              <div className="flex items-center justify-center gap-4">
+                <span className="text-white/80 text-3xl bg-black/20 rounded-full p-3">
+                  <MdFolderZip />
+                </span>
+                <span>{message.fileUrl.split("/").pop()}</span>
+                <span
+                  className="bg-black/20 p-3 text-2xl rounded-full cursor-pointer hover:bg-black/50 transition-all duration-300"
+                  onClick={() => downloadFile(message.fileUrl)}
+                >
+                  <IoMdArrowRoundDown />
+                </span>
+              </div>
+            )}
           </div>
         )}
         <div className="text-xs text-gray-600">
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -16,3 +16,4 @@ export const GET_DM_CONTACTS_ROUTES = `${CONTACTS_ROUTES}/contacts-for-dm`;
 
 export const MESSAGES_ROUTES = "api/messages";
 export const GET_ALL_MESSAGES_ROUTES = `${MESSAGES_ROUTES}/get-messages`;
+export const UPLOAD_FILE_ROUTE = `${MESSAGES_ROUTES}/upload-file`;
