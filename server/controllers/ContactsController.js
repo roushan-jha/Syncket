ca506e53d9c65335ecd937893fcf8a23db713b0e
roushan-jha <jharoushan20@gmail.com>
2025-02-19 19:07:29 +0530

    created getAllContacts api

--- server/controllers/ContactsController.js
+++ server/controllers/ContactsController.js
@@ -3,24 +3,27 @@ import User from "../models/User.js";
 
 export const searchContacts = async (req, res, next) => {
   try {
-   const {searchTerm} = req.body;
+    const { searchTerm } = req.body;
 
-   if(searchTerm === undefined || searchTerm === null) {
-        return res.status(400).send("searchTerm is required.");
-   }
+    if (searchTerm === undefined || searchTerm === null) {
+      return res.status(400).send("searchTerm is required.");
+    }
 
-   const sanitizedSearchTerm = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    const sanitizedSearchTerm = searchTerm.replace(
+      /[.*+?^${}()|[\]\\]/g,
+      "\\$&"
+    );
 
-   const regex = new RegExp(sanitizedSearchTerm, "i");
+    const regex = new RegExp(sanitizedSearchTerm, "i");
 
-   const contacts = await User.find({
-    $and: [
+    const contacts = await User.find({
+      $and: [
         { _id: { $ne: req.userId } },
         {
-            $or: [{ firstName: regex }, { lastName: regex }, { email: regex}],
-        }
-    ],
-   })
+          $or: [{ firstName: regex }, { lastName: regex }, { email: regex }],
+        },
+      ],
+    });
 
     return res.status(200).json({ contacts });
   } catch (err) {
@@ -31,55 +34,73 @@ export const searchContacts = async (req, res, next) => {
 
 export const getContactsForDMList = async (req, res, next) => {
   try {
-   let { userId } = req;
+    let { userId } = req;
 
-   const contacts = await Message.aggregate([
-    {
-      $match: {
-        $or: [{ sender: userId }, { recipient: userId }],
-      },
-    },
-    {
-      $sort: { timestamp: -1 },
-    },
-    {
-      $group: {
-        _id: {
-          $cond: {
-            if: { $eq: ["$sender", userId] },
-            then: "$recipient",
-            else: "$sender",
-          },
+    const contacts = await Message.aggregate([
+      {
+        $match: {
+          $or: [{ sender: userId }, { recipient: userId }],
         },
-        lastMessageTime: { $first: "$timestamp" },
       },
-    },
-    {
-      $lookup: {
-        from: "users",
-        localField: "_id",
-        foreignField: "_id",
-        as: "contactInfo",
+      {
+        $sort: { timestamp: -1 },
       },
-    },
-    {
-      $unwind: "$contactInfo",
-    },
-    {
-      $project: {
-        _id: 1,
-        lastMessageTime: 1,
-        email: "$contactInfo.email",
-        firstName: "$contactInfo.firstName",
-        lastName: "$contactInfo.lastName",
-        image: "$contactInfo.image",
-        color: "$contactInfo.color",
+      {
+        $group: {
+          _id: {
+            $cond: {
+              if: { $eq: ["$sender", userId] },
+              then: "$recipient",
+              else: "$sender",
+            },
+          },
+          lastMessageTime: { $first: "$timestamp" },
+        },
       },
-    },
-    {
-      $sort: { lastMessageTime: -1 },
-    },
-   ]);
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "contactInfo",
+        },
+      },
+      {
+        $unwind: "$contactInfo",
+      },
+      {
+        $project: {
+          _id: 1,
+          lastMessageTime: 1,
+          email: "$contactInfo.email",
+          firstName: "$contactInfo.firstName",
+          lastName: "$contactInfo.lastName",
+          image: "$contactInfo.image",
+          color: "$contactInfo.color",
+        },
+      },
+      {
+        $sort: { lastMessageTime: -1 },
+      },
+    ]);
+
+    return res.status(200).json({ contacts });
+  } catch (err) {
+    console.log({ err });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const users = await User.find(
+      { _id: { $ne: req.userId } },
+      "firstName lastName _id email"
+    );
+
+    const contacts = users.map((user) => ({
+      label: user.firstName ? `${user.firstName} ${user.lastName}` : user.email,
+    }));
 
     return res.status(200).json({ contacts });
   } catch (err) {
