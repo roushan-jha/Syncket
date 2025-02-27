3bd9a4ad504d545eb0d04a8d13951d052e21e6dd
roushan-jha <jharoushan20@gmail.com>
2025-02-27 19:52:21 +0530

    made ui and added methods to receive userChannel on client-side

--- client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -6,33 +6,40 @@ import { RiCloseFill } from "react-icons/ri";
 const ChatHeader = () => {
   const { closeChat, selectedChatType, selectedChatData } = useAppStore();
 
-  console.log(`${HOST}/${selectedChatData.image}`)
+  console.log(`${HOST}/${selectedChatData.image}`);
 
   return (
     <div className="h-[10vh] border-b-2  border-[#2f303b] flex items-center justify-between px-20">
       <div className="flex gap-5 items-center w-full justify-between">
         <div className="flex gap-3 items-center justify-center">
           <div className="w-12 h-12 relative">
-            <Avatar className="h-12 w-12 rounded-full overflow-hidden">
-              {selectedChatData.image ? (
-                <AvatarImage
-                  src={`${HOST}/${selectedChatData.image}`}
-                  alt="profile"
-                  className="object-cover w-full h-full bg-black"
-                />
-              ) : (
-                <div
-                  className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
-                    selectedChatData.color
-                  )}`}
-                >
-                  {selectedChatData.firstName
-                    ? selectedChatData.firstName.split("").shift()
-                    : selectedChatData.email.split("").shift()}
-                </div>
-              )}
-            </Avatar>
+            {selectedChatType === "contact" ? (
+              <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                {selectedChatData.image ? (
+                  <AvatarImage
+                    src={`${HOST}/${selectedChatData.image}`}
+                    alt="profile"
+                    className="object-cover w-full h-full bg-black"
+                  />
+                ) : (
+                  <div
+                    className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                      selectedChatData.color
+                    )}`}
+                  >
+                    {selectedChatData.firstName
+                      ? selectedChatData.firstName.split("").shift()
+                      : selectedChatData.email.split("").shift()}
+                  </div>
+                )}
+              </Avatar>
+            ) : (
+              <div className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full">
+                #
+              </div>
+            )}
           </div>
+          {selectedChatType === "channel" && selectedChatData.name}
           {selectedChatType === "contact" && selectedChatData.firstName
             ? `${selectedChatData.firstName} ${selectedChatData.lastName}`
             : selectedChatData.email}
--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -2,13 +2,21 @@ import { useEffect } from "react";
 import NewDM from "./components/new-dm";
 import ProfileInfo from "./components/profile-info";
 import { apiClient } from "@/lib/api-client";
-import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import {
+  GET_DM_CONTACTS_ROUTES,
+  GET_USER_CHANNELS_ROUTE,
+} from "@/utils/constants";
 import { useAppStore } from "@/store";
 import ContactList from "@/components/ContactList";
 import CreateChannel from "./components/create-channel";
 
 const ContactsContainer = () => {
-  const { directMessagesContacts, setDirectMessagesContacts } = useAppStore();
+  const {
+    directMessagesContacts,
+    setDirectMessagesContacts,
+    channels,
+    setChannels,
+  } = useAppStore();
 
   useEffect(() => {
     const getContacts = async () => {
@@ -21,8 +29,19 @@ const ContactsContainer = () => {
       }
     };
 
+    const getChannels = async () => {
+      const response = await apiClient.get(GET_USER_CHANNELS_ROUTE, {
+        withCredentials: true,
+      });
+
+      if (response.data.channels) {
+        setChannels(response.data.channels);
+      }
+    };
+
     getContacts();
-  }, []);
+    getChannels();
+  }, [setChannels, setDirectMessagesContacts]);
 
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
@@ -42,6 +61,9 @@ const ContactsContainer = () => {
         <div className="flex items-center justify-between pr-10">
           <Title text="Channels" />
           <CreateChannel />
+        </div>
+        <div className="max-h-[40vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={channels} isChannel={true} />
         </div>
       </div>
       <ProfileInfo />
